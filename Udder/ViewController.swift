f40e6d841e6ce96baeb2e869261d5783418dc012
Massimo Fiorentino <m.fiorentino@interiority.com>
2020-02-21 14:58:43 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Massimo Fiorentino on 21/02/2020.
+//  Copyright © 2020 Massimo Fiorentino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
